b0087a795015c4bc057340e7e0f4f9e6f60da014
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2018-12-14 08:47:18 +0100

    add task Day 10 and solution

---
+++ adventofcode/2018/10/index-booy2nd.js
@@ -0,0 +1,4 @@
+// I did it visually -.-
+// see https://codepen.io/Booya/pen/XoXaEr?editors=1111
+
+// approach would be to calculate the point in time where all points are closest to each other and render that image
