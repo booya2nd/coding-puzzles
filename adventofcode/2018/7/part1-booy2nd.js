7fc4816b29a1427855a8f9fd7c4eaa8b7a442fb5
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2018-12-22 13:26:32 +0100

    finally understood 🤦‍ ... my solution used a strictly directed graph which was not asked for

--- adventofcode/2018/7/part1-booy2nd.js
+++ adventofcode/2018/7/part1-booy2nd.js
@@ -1,51 +1,22 @@
-var debug = `
-Step C must be finished before step A can begin.
-Step C must be finished before step F can begin.
-Step A must be finished before step B can begin.
-Step A must be finished before step D can begin.
-Step B must be finished before step E can begin.
-Step D must be finished before step E can begin.
-Step F must be finished before step E can begin.
-`;
-var input = (debug || document.body.textContent).trim().split('\n');
-let nodes = {};
+((input) => {
+  const list = {}, resolved = {};
+  const createNode = name => list[name]||(list[name]={name, deps:[]});
+  input.trim().split('\n')
+  .forEach((line) => {
+    const [, dep, node] = line.match(/Step (\w) must be finished before step (\w) can begin./);
+    createNode(node).deps.push(createNode(dep));
+  });
 
-const sortByName = ({name:a},{name:b}) => (a<b && -1) || (a>b && 1) || 0;
-const createNode = name => { const node={name, parents:[], children:[], done:false}; return nodes[name]=node; };
-input.forEach((line) => {
-  const [,parent, child] = line.match(/Step (\w) must be finished before step (\w) can begin./);
-  const childNode = nodes[child] || createNode(child);
-  const parentNode = nodes[parent] || createNode(parent);
+  const getSortedList = () => Object.keys(list).sort().map(key => list[key]);
+  const isResolved = node => !node.deps.length || node.deps.every(dep => dep.name in resolved);
+  const next = () => getSortedList().filter(isResolved)[0];
 
-  parentNode.children.push(childNode);
-  childNode.parents.push(parentNode);
-});
-
-
-const vRoot = {children: Object.values(nodes).filter(node => !node.parents.length)};
-const sortedNodes = [];
-
-(function sort(node){
-  const {name='', parents = [], children = [], sorted = false} = node;
-  !sorted && children.sort(sortByName) && (node.sorted=true);
-  const ready = !parents.length || parents.every(parent => parent.done);
-  console.log(
-    parents.map(c=>`${c.done?'✅':'✖️'}${c.name}`)+'',
-    '<--', name||'start', '-->',
-    children.map(c=>`${c.done?'✅':'✖️'}${c.name}`)+''
-  );
-  if (!node.done && ready) {
-    node.done=true;
-    name && sortedNodes.push(node);
-    children.forEach(sort);
+  let current;
+  while(current = next()){
+    resolved[current.name] = current;
+    delete list[current.name];
   }
-})(vRoot);
 
-console.log(sortedNodes.map(node => node.name).join(''));
-
-console.log({
-  vRoot,
-  nodes
-});
-
+  console.log(Object.keys(resolved).join(''))
+})(document.body.textContent);
 
