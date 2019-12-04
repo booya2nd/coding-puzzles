38b4bd7ef11a311b5136bdf28d0d77da9350e47b
auerbachstefan <52403576+auerbachstefan@users.noreply.github.com>
2019-12-04 18:32:23 +0100

    even nicer
    even nicer

--- adventofcode/2019/04/auerbachstefan/part2.py
+++ adventofcode/2019/04/auerbachstefan/part2.py
@@ -1,14 +1,12 @@
-def go2(a,b):
-    c=0
-    i=a
-    while i <= b:
-        s = str(i)
-        if sum([s[i]>s[i+1] for i in range(len(s)-1)])==0:
-            if 2 in [len(x) for x in (''.join([s[j] if s[j]==s[j+1] else s[j]+',' for j in range(len(s)-1)])+s[-1]).split(',')]:
-                c=c+1
-        i=i+1
-    return c
-
 input = open('input.txt', 'r').read()
-res = go2(int(input.split('-')[0]),int(input.split('-')[1]))
+res = sum([
+    sum([s[i]>s[i+1] for i in range(len(s)-1)])==0
+     and
+    2 in [len(x) for x in (''.join([s[j] if s[j]==s[j+1] else s[j]+',' for j in range(len(s)-1)])+s[-1]).split(',')]
+    for s in [str(i) for i in range(int(input.split('-')[0]),int(input.split('-')[1])+1)]
+])
 print(res)
+
+
+
+
