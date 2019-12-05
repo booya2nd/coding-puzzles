6d5631f6ae628c81b50d23555af51c870eb2f948
auerbachstefan <52403576+auerbachstefan@users.noreply.github.com>
2019-12-05 23:05:09 +0100

    Update part1.py

--- adventofcode/2019/04/auerbachstefan/part1.py
+++ adventofcode/2019/04/auerbachstefan/part1.py
@@ -1,8 +1,6 @@
-input = open('input.txt', 'r').read()
-res = sum([
-    sum([s[i]>s[i+1] for i in range(len(s)-1)])==0
-     and
-    len(set([x for x in s]))<=5
-    for s in [str(i) for i in range(int(input.split('-')[0]),int(input.split('-')[1])+1)]
-])
+import os
+os.chdir('C:\\Users\\Stefan\\PycharmProjects\\aoc\\04\\auerbachstefan')
+input = [int(i) for i in open('input.txt', 'r').read().split('-')]
+res = sum([sum([s[i] > s[i + 1] for i in range(len(s) - 1)]) == 0 and len(set([x for x in s]))<=5
+    for s in [str(i) for i in range(input[0], input[1] + 1)]])
 print(res)
