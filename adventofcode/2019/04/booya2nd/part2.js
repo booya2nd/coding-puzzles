37f02833e6ef02ebc8ee5fa240c0fc5e58a8080c
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2019-12-05 22:39:44 +0100

    adventofcode day 04 solution part2

--- adventofcode/2019/04/booya2nd/part2.js
+++ adventofcode/2019/04/booya2nd/part2.js
@@ -1,5 +1,6 @@
 const source="136760-595730";
 let [i,l]=source.split('-'),c=0;
-const r=/(\d)\1/,r2=/^(\d)\1*(?=.*(\d)\2{2})/,v=(n,b=!0)=>([...n+''].reduce((p,n)=>(b&=n>=p,n)),!!b);
-for(;i*1<=l*1;i++)r.test(i)&&!r2.test(i)&&v(i)&&++c&&console.log(i);
-console.log(c);  // its wrong ... should be ... 1264
+const r=n=>[...(n.matchAll(/(\d)\1+/g)||[])].find(([{length:l}])=>l===2)
+const v=(n,b=!0)=>([...n+''].reduce((p,n)=>(b&=n>=p,n)),!!b);
+for(;i*1<=l*1;i++)r(i+'')&&v(i)&&++c;
+console.log(c);
