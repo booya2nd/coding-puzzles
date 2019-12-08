008bda0b68007cad5d36b56cbd6eeb628fa81117
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2019-12-08 02:41:34 +0100

    adventofcode day 05 solution part1 WIP wrong

---
+++ adventofcode/2019/05/booya2nd/part1.js
@@ -0,0 +1,39 @@
+const source = require('fs').readFileSync('input.txt', 'utf8').trim().split(',');
+const INPUT = 1;
+
+function operate(state) {
+  const MODES = {
+    0: int => state[int],
+    1: int => int,
+  };
+  const $ = (value, mode=0) => MODES[mode](value);
+  const OPS = {
+    1: (arg1, arg2, stor) => { state[stor] = arg1 + arg2 },
+    2: (arg1, arg2, stor) => { state[stor] = arg1 * arg2 },
+    3: (stor) => { state[stor] = INPUT },
+    4: (arg1) => { console.log(arg1) }
+  };
+
+  for (let i=0; state[i] !== 99; i++) {
+    // let debug = state.slice(Math.max(i-10,0),i+4);
+    // debug.splice(debug.length-4,1,'>'+state[i]);
+    // console.log(i,': ...'+debug.join(' ')+'...');
+    const opinstr = state[i]+'';
+    const opcode = opinstr.slice(-2);
+    const argModes = [...opinstr.replace(opcode, '')].reverse();
+    const op = OPS[opcode];
+    console.log(JSON.stringify(state.slice(0,i+1)));
+    console.log(`('pos ', ${i}, 'instr ', '${opinstr}', 'opcode', ${opcode})`);
+    if (op) {
+      const args = state.slice(i+1, i+op.length+1);
+      const argValues = args.map((value, i) => $(value, argModes[i]));
+      console.log(`${opinstr} ${args.join(' ')}@${argModes}`,`=> OPS[${opcode}](${argValues})`);
+      console.log(`OPS[${opcode}](${argValues})`);
+      op(i, ...argValues);
+      i += op.length;
+    }
+  }
+  return state;
+}
+
+console.log(operate(source.map(Number)));
