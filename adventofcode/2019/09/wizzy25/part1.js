87171b51ac98a94c8ae0f20723b008309f447895
Williams Omayuku <womayuku@gmail.com>
2019-12-12 09:50:11 +0100

    WIP: day 9 - part 1

---
+++ adventofcode/2019/09/wizzy25/part1.js
@@ -0,0 +1,75 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const input = readFileSync(join(__dirname, 'input.txt'), 'utf8').trim().split(',').map(Number);
+let base = 0;
+const operations = {
+  1: (i, pos, x, y) => {
+    input[pos] = x + y;
+    return  i + 4;
+  },
+  2: (i, pos, x, y) => {
+    input[pos] = x * y;
+    return i + 4;
+  },
+  3: (i, pos) => {
+    input[pos] = 1;
+    return i + 2;
+  },
+  4: (i, pos) => {
+    console.log(input[pos]);
+    return i + 2;
+  },
+  5: (i, pos, x, y) => {
+    return x ? y : i + 3;
+  },
+  6: (i, pos, x, y) => {
+    return !x ? y : i + 3;
+  },
+  7: (i, pos, x, y) => {
+    input[pos] = x < y ? 1 : 0;
+    return i + 4;
+  },
+  8: (i, pos, x, y) => {
+    input[pos] = x === y ? 1 : 0;
+    return i + 4;
+  },
+  9: (i, offset) => {
+    base += offset;
+    return i + 2;
+  }
+}
+
+const modes = {
+  0: (val, list) => list[val],
+  1: (val, list) => val,
+  // 2: (val, list) => val + base,
+  2: (val, list) => val + base,
+}
+
+const shouldTerminate = val => val === 99;
+
+let i = 0;
+while (i < input.length) {
+  const op = Number(input[i].toString().slice(-2));
+  if (shouldTerminate(op)) break;
+
+  const [fpMode = 0, spMode = 0] = input[i].toString().slice(0, -2).split('').reverse().map(Number);
+  let pos, val1, val2;
+
+  const values = input.slice(i + 1, i + 4)
+  if (op === 1 || op === 2 || op === 7 || op === 8) { // 3 params
+    val1 = modes[fpMode](values[0], input);
+    val2 = modes[spMode](values[1], input);
+    pos = values[2];
+  } else if (op === 3 || op === 4 || op === 9) { // 1 param
+    // pos = values[0];
+    pos = modes[fpMode](values[0], input);
+  } else { // 2 params
+    val1 = modes[fpMode](values[0], input);
+    val2 = modes[spMode](values[1], input);
+  }
+
+  i = operations[op](i, pos, val1, val2);
+  // i = op in operations ? operations[op](i, pos, val1, val2) : ++i;
+}
