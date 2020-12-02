cd6c5d675fab63bbb3b24a97b204955e8f290f25
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2020-12-02 20:21:59 +0100

    fix breaks from day 1

--- adventofcode/2020/01/booya2nd/part1.js
+++ adventofcode/2020/01/booya2nd/part1.js
@@ -1,11 +1,11 @@
 const input = require('fs').readFileSync('input.txt', 'utf8').split('\n');
 const nums = input.map(Number);
 
-for (let i=0, l=nums.length; i<l; i++) {
+a:for (let i=0, l=nums.length; i<l; i++) {
   for (let j=i+1; j<l; j++) {
     if (nums[i]+nums[j] === 2020) {
       console.log(nums[i] * nums[j]);
-      break;
+      break a;
     }
   }
 }
--- adventofcode/2020/01/booya2nd/part2.js
+++ adventofcode/2020/01/booya2nd/part2.js
@@ -1,12 +1,12 @@
 const input = require('fs').readFileSync('input.txt', 'utf8').split('\n');
 const nums = input.map(Number);
 
-for (let i=0, l=nums.length; i<l; i++) {
+a:for (let i=0, l=nums.length; i<l; i++) {
   for (let j=i+1; j<l; j++) {
     for (let k=j+1; k<l; k++) {
       if (nums[i]+nums[j]+nums[k] === 2020) {
         console.log(nums[i]*nums[j]*nums[k]);
-        break;
+        break a;
       }
     }
   }
