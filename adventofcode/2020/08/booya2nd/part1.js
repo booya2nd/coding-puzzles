58d63a75a47b7477f1b64b0ac2bfa45b34347c2a
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2020-12-08 22:32:10 +0100

    day 8

---
+++ adventofcode/2020/08/booya2nd/part1.js
@@ -0,0 +1,25 @@
+const input = require('fs').readFileSync('input.txt', 'utf8').trim().split('\n');
+
+function run(input, acc = 0, step= 0, mod=x=>x){
+  const CACHE = [];
+
+  const OPS = {
+    nop: () => { return },
+    acc: val => { acc += val },
+    jmp: (val, step) => { return val + step }
+  }
+
+  let line;
+  while((line = input[step]) && !CACHE.includes(step)) {
+    CACHE.push(step);
+
+    let [, opcode, val] = line.match(/(\w+) (.\d+)/);
+    ({ opcode, val, step, acc } = mod({ opcode, val: val*1, step, acc, CACHE, }));
+    step = OPS[opcode](val*1, step) ?? step+1;
+  }
+
+  return { acc, step, CACHE, exit: input.length-step?1:0 }
+}
+
+
+console.log(run(input))
