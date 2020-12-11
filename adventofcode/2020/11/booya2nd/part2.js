ba3c27cd9088c542f0871050f469dcf661d50761
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2020-12-11 23:35:01 +0100

    day 11 p2

---
+++ adventofcode/2020/11/booya2nd/part2.js
@@ -0,0 +1,55 @@
+const input = require('fs').readFileSync('input.txt', 'utf8').trim().split('\n');
+
+let state = input.map(line => [...line]);
+function getAdjacentOcc(state, x, y) {
+  const checks = [
+    [-1,-1], [-1,0], [-1,1],
+    [0,-1], [0,1],
+    [1,-1], [1,0], [1,1],
+  ]
+
+  return checks.reduce((acc, [yi,xi]) => {
+    let i=0;
+    while(++i) {
+      const v = state[y+yi*i]?.[x+xi*i]; acc.push(v)
+      if (!v || v !== '.') return acc;
+    }
+  }, []).filter(v => v === '#')
+}
+
+function gen(state) {
+  const newState = JSON.parse(JSON.stringify(state));
+  state.forEach((line, y) => {
+    line.forEach((v, x) => {
+      switch(v) {
+        case 'L':
+          const isFree = getAdjacentOcc(state, x, y).length === 0;
+          isFree && (newState[y][x] = '#');
+          break;
+        case '#':
+          const isFull = getAdjacentOcc(state, x, y).length >= 5;
+          isFull && (newState[y][x] = 'L');
+          break;
+      }
+    })
+  });
+  return newState;
+}
+
+function draw(state) {
+  const str = state.map(line => line.join('')).join('\n');
+  console.log(str);
+}
+
+let i=0;
+while(++i) {
+  const prevState = state;
+  state = gen(state);
+  if (prevState+'' === state+'') break;
+  console.log(`\n\n GEN ${i}>`);
+  draw(state);
+}
+
+console.log(
+  (state+'').match(/#/g)?.length
+);
