eae722bf826107d5a4d173786e0a951791ae822b
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2020-12-20 21:41:40 +0100

    day 19 p1 wip dyn. programming fail ...

--- adventofcode/2020/19/README.txt
+++ adventofcode/2020/19/README.txt
@@ -15,7 +15,8 @@ The rules for valid messages (the top part of your puzzle input) are numbered an
 3: "b"
 Some rules, like 3: "b", simply match a single character (in this case, b).
 
-The remaining rules list the sub-rules that must be followed; for example, the rule 0: 1 2 means that to match rule 0, the text being checked must match rule 1, and the text after the part that matched rule 1 must then match rule 2.
+The remaining rules list the sub-rules that must be followed;
+for example, the rule 0: 1 2 means that to match rule 0, the text being checked must match rule 1, and the text after the part that matched rule 1 must then match rule 2.
 
 Some of the rules have multiple lists of sub-rules separated by a pipe (|). This means that at least one list of sub-rules must match. (The ones that match might be different each time the rule is encountered.) For example, the rule 2: 1 3 | 3 1 means that to match rule 2, the text being checked must match rule 1 followed by rule 3 or it must match rule 3 followed by rule 1.
 
--- adventofcode/2020/19/booya2nd/part1.js
+++ adventofcode/2020/19/booya2nd/part1.js
@@ -1,35 +1,38 @@
 const [_rules, _str] = require('fs').readFileSync('input.txt', 'utf8').trim().split('\n\n')
 
-const parsedRules = _rules
-.replace(/\d+: /g,'').trim().split('\n')
-.map(line =>
-  line.split(' | ')
-  .map(rule => rule.match(/\w/g))
-);
+const parsedRules = _rules.trim().split('\n').reduce((acc, line) => {
+  const [,i,rules] = line.match(/^(\d+): (.*)/);
+  acc[i] = rules.split(' | ').map(rule => rule.match(/\w+/g));
+  return acc;
+}, []);
 
-function isValid(str, ruleIndex = '0'){
-  const ORrules = parsedRules[ruleIndex];
-  console.group(`isValid("${str.join('')}", ${ruleIndex})`, JSON.stringify(ORrules));
-  const r = ORrules.some(ANDrules => {
-    // console.log(':: :: OR');
-    return ANDrules.every(x => {
-      // console.log(':: :: :: AND');
-      if (isNaN(x*1)) {
-        console.log(x,'===',str[0]);
-        return x === str[0]
+console.log = x => x;
+function check(str, ruleIndex = 0) {
+  const disjuncts = parsedRules[ruleIndex];
+  console.group(`isValid("${str}", ${ruleIndex})`, disjuncts.map(x=>x.join(' ')));
+  const ok = disjuncts.some( conjuncts => {
+    return conjuncts.every( rule => {
+      const isRef = rule in parsedRules;
+      if (isRef) {
+        console.log('... goto ',rule);
+        const { ok, left } = check(str, rule);
+        if (ok) str = left;
+        return ok;
       } else {
-        return isValid(str, x)
+        const matches = rule === str[0];
+        console.log('... comp ', rule, '===', str[0], matches ? '✅ ' : '❌ ');
+        matches && (str = str.slice(1), console.log(`... str is now "${str}"`));
+        return matches
       }
-    }) && str.splice(0,1)
+    })
   });
   console.groupEnd();
-  return r;
-};
+  return { ok, left: str, isValid() { return this.ok } }
+}
 
-console.log(
-isValid([...'ababbb']))
+console.info(Object.fromEntries(Object.entries(parsedRules)));
 
 
-0&&console.log(
-  _str.split('\n').filter(str => isValid([...str]))
+console.info(
+  _str.split('\n').filter((str,i) => check(str).isValid())
 );
