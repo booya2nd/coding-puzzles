13b41d85ad33757cf768aeeb8a261b559c876ad4
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2021-12-02 17:22:39 +0100

    2021 day 2 part1

--- adventofcode/2021/2/booya2nd/part1.js
+++ adventofcode/2021/2/booya2nd/part1.js
@@ -1,1 +1,15 @@
 const text = require('fs').readFileSync('input.txt', 'utf8').trim();
+const input = text.split('\n');
+
+const position = [0,0];
+const DIRS = {forward: [1,0], down: [0,1], up: [0,-1]};
+function calc(instr) {
+  const [dir,n] = instr.split(' ');
+  const [x,y] = DIRS[dir].map(i => i*n);
+  position[0] += x;
+  position[1] += y;
+}
+
+input.forEach(calc);
+
+console.log(position[0] * position[1]);
