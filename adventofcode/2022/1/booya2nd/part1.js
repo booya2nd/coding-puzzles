5f9d284585309a8ae5c86a3134423d1c979ec16f
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2022-12-01 23:50:42 +0100

    chore: day 1

--- adventofcode/2022/1/booya2nd/part1.js
+++ adventofcode/2022/1/booya2nd/part1.js
@@ -1,1 +1,7 @@
-// tbd
+const text = require('fs').readFileSync('input.txt', 'utf8').trim();
+const input = text.split('\n');
+
+let x=0,max=0;
+input.map(n=>n?(x+=n*1):(max=Math.max(x,max,x=0)));
+
+console.log(max);
--- adventofcode/2022/1/booya2nd/part2.js
+++ adventofcode/2022/1/booya2nd/part2.js
@@ -1,1 +1,8 @@
-// td
+const text = require('fs').readFileSync('input.txt', 'utf8').trim();
+const input = text.split('\n');
+
+let x=0,max=[];
+input.map(n=>n?(x+=n*1):(max.push(x),x=0));
+max.sort((a,b)=>b-a);
+
+console.log(max.slice(0,3).reduce((a,b)=>a+b));
