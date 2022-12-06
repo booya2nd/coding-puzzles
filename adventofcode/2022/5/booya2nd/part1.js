c359924a13ff352cb2823aae74d50398df4c5c5a
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2022-12-06 23:52:05 +0100

    chore: day 5

---
+++ adventofcode/2022/5/booya2nd/part1.js
@@ -0,0 +1,23 @@
+const text = require('fs').readFileSync('input.txt', 'utf8').trimEnd();
+const [layout,moves] = text.split('\n\n').map(part => part.split('\n'));
+const CHR_WIDTH = '[X] '.length;
+
+// store (and transpose) parsed crates
+const crates = [];
+layout.slice(0,-1).forEach((line, row) => {
+    const values = line.match(/\w| {3,4}/g);
+    for(let i=0; i<line.length; i+=CHR_WIDTH) {
+        const col = i/CHR_WIDTH;
+        const v = values[col].trim();
+        v && (crates[col] ||= []).unshift(v);
+    }
+});
+
+// move the crates
+moves.forEach((moveCmd) => {
+    const [, amount, from, to] = moveCmd.match(/move (\d+) from (\d+) to (\d+)/);
+    const picked = crates[from-1].splice(-amount,amount).reverse();
+    crates[to-1].push(...picked);
+});
+
+console.log(crates.map(stack => stack.at(-1)).join(''));
