bfe4a41faf8c0b252724979141ad15c407f61d9f
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2022-12-08 02:03:22 +0100

    chore: day 7 part 1

---
+++ adventofcode/2022/7/booya2nd/part1.js
@@ -0,0 +1,50 @@
+const text = require('fs').readFileSync('input.txt', 'utf8').trim();
+const input = text.split('\n');
+
+const sum = arr => arr.reduce((a,b) => a+b, 0);
+const Node = (name, size) => ({
+    name, p:null, c:{}, size: size*1, isDir: size === 'dir',
+    add(n){ n.parent = this; this.c[n.name] = n },
+    get total(){ return this.size || sum(Object.values(this.c).map(c => c.total))  },
+    walk(cb){ Object.values(this.c).forEach((c,i,arr) => { cb(c,i,arr); c.walk(cb) })},
+    filter(predicate){
+        const result = [];
+        this.walk((n,i,arr) => { predicate(n,i,arr) && result.push(n) });
+        return result;
+    }
+})
+const state = {
+    lines: input,
+    tree: {
+        '/': Node('/'),
+        get '..'(){ return state.cwd.parent }
+    },
+    cwd: null
+};
+
+const COMMANDS = {
+    'cd': (target) => {
+        state.cwd = state.tree[target] || state.cwd.c[target];
+    },
+    'ls': () => {
+        const n = state.lines.findIndex(line => line.startsWith('$'));
+        const output = state.lines.splice(0, n === -1 ? Infinity : n);
+        output.forEach((line) => {
+            const [size, name] = line.split(' ');
+            state.cwd.add(Node(name, size))
+        })
+    },
+}
+
+while(state.lines.length) {
+    const [line] = state.lines.splice(0,1);
+    const [,cmd,args] = line.match(/\$ (\w+)(?: (.*))?/);
+    COMMANDS[cmd](args);
+}
+
+console.log(sum(
+    state.tree['/']
+        .filter(n => n.isDir && n.total < 1e5)
+        .map(n => n.total)
+));
+
