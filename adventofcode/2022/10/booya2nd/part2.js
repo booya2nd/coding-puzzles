d02ab30689f38a61cc80f6b09a1ac4c997a52473
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2023-01-02 18:08:37 +0100

    chore: day 10 part 2

--- adventofcode/2022/10/booya2nd/part2.js
+++ adventofcode/2022/10/booya2nd/part2.js
@@ -1,16 +1,20 @@
 const text = require('fs').readFileSync('input.txt', 'utf8').trim();
 const instr = text.split('\n').map(l => l.split(' '))
 
-const INSPECT_SIGNALS = [20,60,100,140,180,220];
-
-let X=1;
-const log = [];
+let X=1; const log = [];
 while (instr.length) {
     const [cmd, arg] = instr.shift();
     log.push(X);
     if (cmd === 'addx') { log.push(X); X+=arg*1; }
 }
 
-const sum = INSPECT_SIGNALS.map(s => log[s-1] * s).reduce((a,b)=>a+b);
+const SCREEN_W = 40;
+const SPRITE_X = '###'.length/2|0;
 
-console.log(sum);
+while(log.length){
+    console.log(
+        log.splice(0,SCREEN_W)
+            .map((X,px) => Math.abs(px-X)<=SPRITE_X ? '#' : '.')
+            .join('')
+    );
+}
