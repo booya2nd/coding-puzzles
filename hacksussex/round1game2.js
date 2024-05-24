1eca427dee238169da776ed08e7ce514e94147a8
Marcel Lucas <marcel.lucas@sevenonemedia.de>
2024-05-24 08:14:49 +0200

    hacksussex: r1g2

---
+++ hacksussex/round1game2.js
@@ -0,0 +1,14 @@
+function hasAllNumbers(arr) {
+
+    for (nums of arr) {
+       const len = nums.length;
+       nums.sort((a,b) => a-b);
+       for (let i=0; i<len; i++) {
+           if (nums[i] !== i+1) return false;
+       }
+    }
+    return true;
+}
+
+const r = hasAllNumbers([[1,2,3,4],[2,3,1],[2,1,3]]); // invalid
+console.log( r);
