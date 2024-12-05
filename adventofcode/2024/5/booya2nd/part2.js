611b5bb41a6c7ece5dffe9a7505ca19b364adb96
Marcel Lucas <marcel.lucas@seven.one>
2024-12-05 16:43:02 +0100

    refactor(adventofcode/2024): day 5 part2 (no global score)

--- adventofcode/2024/5/booya2nd/part2.js
+++ adventofcode/2024/5/booya2nd/part2.js
@@ -2,14 +2,13 @@ const text = require('fs').readFileSync('input.txt', 'utf8').trim();
 // map map map 🤮
 const [order, updates] = text.split('\n\n').map(s => s.split('\n').map(l => l.split(/[,|]/).map(Number)));
 const g={}; order.map(([a,b])=>(g[a]||={_:a})[b]=g[b]||={_:b});
-const s={}; Object.values(g).map(n=>s[n._]=Object.keys(n).length);
 
 let result = 0;
 for (const u of updates) {
   let x; const ok = u.every(n=>x=(x??g)[n]);
   if (!ok) {
-    const _s={}; u.map(_ => _s[_]=Object.keys(g[_]).filter(_=>u.includes(+_)).length);
-    result += u.sort((a,b) => _s[b]-_s[a])[u.length/2|0];
+    const s={}; u.map(_=>s[_]=Object.keys(g[_]).filter(_=>u.includes(+_)).length);
+    result += u.sort((a,b) => s[b]-s[a])[u.length/2|0];
   }
 }
 
