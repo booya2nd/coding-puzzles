a16f974e81e800665e65c536563c8f0eb57e0012
Vitali <vitali.stsepaniuk@seven.one>
2024-12-06 15:21:36 +0100

    refactor: day06

--- adventofcode/2024/6/vitali/src/main.rs
+++ adventofcode/2024/6/vitali/src/main.rs
@@ -15,17 +15,20 @@ fn main() -> Result<()> {
 fn part1(input: &str) -> usize {
     let grid = parse(input);
 
-    get_visited(&grid, find_start(&grid, &'^')).len()
+    solve(&grid, find_start(&grid, &'^'), (usize::MAX, usize::MAX))
+        .0
+        .len()
 }
 
 fn part2(input: &str) -> usize {
     let grid = parse(input);
     let start = find_start(&grid, &'^');
 
-    get_visited(&grid, start)
-        .iter()
+    solve(&grid, start, (usize::MAX, usize::MAX))
+        .0
+        .into_iter()
         .par_bridge()
-        .filter(|obstacle| **obstacle != start && has_loop(&grid, start, **obstacle))
+        .filter(|obstacle| *obstacle != start && solve(&grid, start, *obstacle).1)
         .count()
 }
 
@@ -42,44 +45,35 @@ fn find_start<T: Eq>(grid: &Grid<T>, start: &T) -> (usize, usize) {
         .unwrap()
 }
 
-fn get_visited(grid: &Grid<char>, (y, x): (usize, usize)) -> HashSet<(usize, usize)> {
+fn solve(
+    grid: &Grid<char>,
+    (y, x): (usize, usize),
+    (oy, ox): (usize, usize),
+) -> (HashSet<(usize, usize)>, bool) {
     let mut visited = HashSet::new();
     let (mut dx, mut dy) = (0, -1);
     let (mut y, mut x) = (y as isize, x as isize);
 
-    loop {
-        visited.insert((y as usize, x as usize));
-        let (nx, ny) = (x + dx, y + dy);
-
-        match grid.get(ny, nx) {
-            Some('#') => {
-                (dx, dy) = (-dy, dx);
-            }
-            None => break,
-            _ => {
-                (x, y) = (nx, ny);
-            }
-        }
-    }
-
-    visited
-}
-
-fn has_loop(grid: &Grid<char>, (y, x): (usize, usize), (oy, ox): (usize, usize)) -> bool {
-    let mut visited = HashSet::new();
-    let (mut dx, mut dy) = (0, -1);
-    let (mut y, mut x) = (y as isize, x as isize);
-
+    let mut not_quit = true;
     while visited.insert((y, x, dx, dy)) {
         (y, x, dx, dy) = match grid.get(y + dy, x + dx) {
             Some('#') => (y, x, -dy, dx),
             Some('.') if oy as isize == y + dy && ox as isize == x + dx => (y, x, -dy, dx),
-            None => return false,
+            None => {
+                not_quit = false;
+                break;
+            }
             _ => (y + dy, x + dx, dx, dy),
         }
     }
 
-    true
+    (
+        visited
+            .iter()
+            .map(|(y, x, _, _)| (*y as usize, *x as usize))
+            .collect(),
+        not_quit,
+    )
 }
 
 #[cfg(test)]
