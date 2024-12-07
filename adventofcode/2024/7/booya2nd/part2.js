19c65b98727943d7f0603d8a18cf48d3873fc724
Marcel Lucas <marcel.lucas@seven.one>
2024-12-07 14:44:26 +0100

    feat(adventofcode/2024): day 7 part1 & part2

---
+++ adventofcode/2024/7/booya2nd/part2.js
@@ -0,0 +1,23 @@
+let text = require('fs').readFileSync('input.txt', 'utf8').trim();
+const input = text.split('\n');
+
+const ops = [(a,b)=>a+b,(a,b)=>a*b,(a,b)=>+`${a}${b}`];
+let result = 0;
+for (const l of input) {
+  const [eq,...nums] = l.match(/\d+/g);
+  let ol=ops.length, nl=nums.length-1, opsi=ol**nl, x=false;
+  while(opsi-->0 && !x) {
+    const op = opsi.toString(ol).padStart(nl,'0').split('').map(i=>ops[i]);
+    let r=+nums[0];
+    for (let i=0; i<op.length; i++) {
+      r = op[i](r, +nums[i+1])
+      if (r > +eq) break;
+    }
+    if (r === +eq) {
+      result += r;
+      break;
+    }
+  }
+}
+
+console.log(result);
