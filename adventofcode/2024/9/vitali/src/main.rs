9b81581a30d176576a0dea4bae3688d8dff25e81
Vitali <vitali.stsepaniuk@seven.one>
2024-12-09 17:16:05 +0100

    refactor: day09

--- adventofcode/2024/9/vitali/src/main.rs
+++ adventofcode/2024/9/vitali/src/main.rs
@@ -1,6 +1,30 @@
 use anyhow::Result;
 use std::collections::VecDeque;
-use std::fmt::{Debug, Formatter};
+
+#[derive(Clone)]
+enum Entry {
+    Space(usize),
+    File(usize, usize),
+}
+
+impl Entry {
+    fn checksum(&self, position: usize) -> usize {
+        match self {
+            Entry::Space(_) => 0,
+            Entry::File(id, len) => {
+                // Sum of arithmetic sequence
+                (position + position + len - 1) * len / 2 * id
+            }
+        }
+    }
+
+    fn size(&self) -> usize {
+        match self {
+            Entry::Space(size) => *size,
+            Entry::File(_, size) => *size,
+        }
+    }
+}
 
 fn main() -> Result<()> {
     let data = std::fs::read_to_string("input.txt")?;
@@ -11,169 +35,108 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-#[derive(Debug, Clone)]
-struct File(u8, usize);
-
-#[derive(Debug, Clone)]
-enum Entry {
-    Space,
-    File(usize),
-}
-
 fn part1(input: &str) -> usize {
-    let disk = parse(input);
+    let mut disk = parse(input).into_iter().collect::<VecDeque<_>>();
 
-    let mut free_space = vec![];
-    let mut files = vec![];
-    let mut id = 0;
-
-    for (idx, size) in disk.iter().enumerate() {
-        if idx % 2 == 0 {
-            files.push(File(*size, id));
-            id += 1;
-        } else {
-            free_space.push(*size);
-        }
-    }
-
-    if free_space.len() != files.len() {
-        free_space.push(0);
-    }
-
-    let mut result = files
-        .iter()
-        .zip(free_space.iter())
-        .flat_map(|(file, &size)| {
-            let mut tmp = vec![Entry::File(file.1); file.0 as usize];
-            tmp.extend(vec![Entry::Space; size as usize]);
-            tmp
-        })
-        .collect::<VecDeque<_>>();
-
-    let mut i = 0;
+    let mut pos = 0;
     let mut checksum = 0;
 
-    while let Some(entry) = result.pop_front() {
+    while let Some(entry) = disk.pop_front() {
         match entry {
-            Entry::File(f) => {
-                checksum += f * i;
+            Entry::File(_, file_size) => {
+                checksum += entry.checksum(pos);
+                pos += file_size;
             }
-            Entry::Space => {
-                while !result.is_empty() {
-                    if let Some(Entry::File(f)) = result.pop_back() {
-                        checksum += f * i;
+            Entry::Space(space_size) => {
+                while let Some(entry) = disk.pop_back() {
+                    if let Entry::File(id, file_size) = entry {
+                        if space_size < file_size {
+                            disk.push_back(Entry::File(id, file_size - space_size));
+                        } else if space_size != file_size {
+                            disk.push_front(Entry::Space(space_size - file_size));
+                        }
+
+                        checksum += Entry::File(id, file_size.min(space_size)).checksum(pos);
+                        pos += file_size.min(space_size);
+
                         break;
                     }
                 }
             }
         }
-
-        i += 1;
     }
 
     checksum
-}
-
-#[derive(Clone)]
-enum Entry2 {
-    Space(usize),
-    File(usize, usize),
-}
-
-impl Debug for Entry2 {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Entry2::Space(size) => write!(f, "{}", ".".repeat(*size)),
-            Entry2::File(id, size) => write!(f, "{}", id.to_string().repeat(*size)),
-        }
-    }
-}
-
-impl Entry2 {
-    fn checksum(&self, position: usize) -> usize {
-        match self {
-            Entry2::Space(_) => 0,
-            Entry2::File(id, len) => (0..*len).map(|i| id * (position + i)).sum(),
-        }
-    }
-
-    fn size(&self) -> usize {
-        match self {
-            Entry2::Space(size) => *size,
-            Entry2::File(_, size) => *size,
-        }
-    }
 }
 
 fn part2(input: &str) -> usize {
-    let disk = parse(input);
-    let mut result = vec![];
-    let mut id = 0;
+    let mut disk = parse(input);
 
-    for (idx, size) in disk.iter().enumerate() {
-        if idx % 2 == 0 {
-            result.push(Entry2::File(id, *size as usize));
-            id += 1;
-        } else {
-            result.push(Entry2::Space(*size as usize));
-        }
-    }
+    let len = disk.len();
+    fragment(&mut disk, len - 1);
 
-    let len = result.len();
-
-    fragment(&mut result, len - 1);
-
-    let mut pos = 0;
-    let mut checksum = 0;
-    for entry in result.iter() {
-        if matches!(entry, Entry2::File(_, _)) {
-            checksum += entry.checksum(pos);
-        }
-
-        pos += entry.size();
-    }
-
-    checksum
+    disk.iter()
+        .fold((0, 0), |(checksum, position), entry| {
+            (
+                checksum
+                    + matches!(entry, Entry::File(_, _))
+                        .then(|| entry.checksum(position))
+                        .unwrap_or(0),
+                position + entry.size(),
+            )
+        })
+        .0
 }
 
-fn fragment(input: &mut Vec<Entry2>, index: usize) {
+fn fragment(input: &mut Vec<Entry>, index: usize) {
     if index == 0 {
         return;
     }
 
-    let Entry2::File(id, size) = input[index] else {
+    let entry = &input[index];
+    let Entry::File(_, file_size) = input[index] else {
         fragment(input, index - 1);
         return;
     };
 
-    // Find space
-    for i in 0..index {
-        match input[i] {
-            Entry2::Space(space) if space >= size => {
-                let remaining = space - size;
-                input[i] = Entry2::File(id, size);
+    if let Some((space_index, space_size)) =
+        input[..index]
+            .iter()
+            .enumerate()
+            .find_map(|(i, entry)| match entry {
+                Entry::Space(size) if *size >= file_size => Some((i, *size)),
+                _ => None,
+            })
+    {
+        let remaining = space_size - file_size;
+        input[space_index] = entry.clone();
 
-                let mut shift = 0;
-                if remaining > 0 {
-                    input.insert(i + 1, Entry2::Space(remaining));
-                    shift = 1;
-                }
-
-
-                input.remove(index + shift);
-                input.insert(index + shift, Entry2::Space(size));
-
-                break;
-            }
-            _ => {}
+        input.remove(index);
+        if remaining > 0 {
+            input.insert(space_index + 1, Entry::Space(remaining));
         }
+
+        input.insert(index, Entry::Space(file_size));
     }
 
     fragment(input, index - 1);
 }
 
-fn parse(input: &str) -> Vec<u8> {
-    input.as_bytes().iter().map(|c| c - b'0').collect()
+fn parse(input: &str) -> Vec<Entry> {
+    input
+        .as_bytes()
+        .iter()
+        .map(|c| c - b'0')
+        .enumerate()
+        .scan(0, |id, (idx, size)| {
+            Some(if idx % 2 == 0 {
+                *id += 1;
+                Entry::File(*id - 1, size as usize)
+            } else {
+                Entry::Space(size as usize)
+            })
+        })
+        .collect()
 }
 
 #[cfg(test)]
