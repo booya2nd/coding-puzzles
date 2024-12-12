9f3702e9291d0a9b14fdace30c5cf33afef4d866
Marcel Lucas <marcel.lucas@seven.one>
2024-12-12 10:17:56 +0100

    feat(adventofcode/2024): day 12 part1

---
+++ adventofcode/2024/12/booya2nd/part1.js
@@ -0,0 +1,18 @@
+let text=require('fs').readFileSync('input.txt', 'utf8').trim();
+let l=text.length,i=l,w=text.indexOf('\n')+1,r={},v={},dir=[-w,1,w,-1];
+
+while(i-->0){
+  if (v[i] || text[i] === '\n') continue;
+  let c=text[i],a=0,p=0,s=[i],pos,n;
+
+  while (s.length) {
+    if (v[pos=s.pop()]) continue;
+    v[pos]=1;a++;
+    for (let d of dir)
+      (n=pos+d)<0 || n>=l || text[n]==='\n' || text[n]!==c ? p++ : !v[n]&&s.push(n);
+  }
+
+  r[c]=(r[c]||0)+a*p;
+}
+
+console.log(Object.values(r).reduce((s,v)=>s+v));
