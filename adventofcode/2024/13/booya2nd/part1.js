c387c0fcaebe54d08c6f131e627b2cf4111500ed
Marcel Lucas <marcel.lucas@seven.one>
2024-12-13 14:45:33 +0100

    refactor(adventofcode/2024): day 13 part1 & part2

--- adventofcode/2024/13/booya2nd/part1.js
+++ adventofcode/2024/13/booya2nd/part1.js
@@ -1,16 +1,16 @@
 const text=require('fs').readFileSync('input.txt', 'utf8').trim();
 console.time();
 const input = [...text.matchAll(/X.(\d+).*?(\d+)\n.*?(\d+).*?(\d+)\n.*?(\d+).*?(\d+)/g)];
-const tokens=[];
+let tokens=0;
 
 for (const [_, ...i] of input) {
   let [xA, yA, xB, yB, x, y]=i.map(Number), det=xA*yB - yA*xB;
-  if (det === 0) continue;
+  if (!det) continue;
   const A = (x*yB - y*xB) / det;
   const B = (-x*yA + y*xA) / det;
 
-  if (A>=0 && B>=0 && A===(A|0) && B===(B|0)) tokens.push(A*3 + B);
+  if (A>=0 && B>=0 && A===(A|0) && B===(B|0)) tokens+=A*3+B;
 }
 
 console.timeEnd();
-console.log(tokens.reduce((a,b)=>a+b));
+console.log(tokens);
--- adventofcode/2024/13/booya2nd/part2.js
+++ adventofcode/2024/13/booya2nd/part2.js
@@ -1,17 +1,18 @@
 const text=require('fs').readFileSync('input.txt', 'utf8').trim();
 console.time();
 const input = [...text.matchAll(/X.(\d+).*?(\d+)\n.*?(\d+).*?(\d+)\n.*?(\d+).*?(\d+)/g)];
-const tokens=[];
 const off = 10000000000000;
+let tokens=0;
+
 for (const [_, ...i] of input) {
   let [xA, yA, xB, yB, _x, _y]=i.map(Number), det=xA*yB - yA*xB;
   const x = _x + off, y = _y + off;
-  if (det === 0) continue;
+  if (!det) continue;
   const A = (x*yB - y*xB) / det;
   const B = (-x*yA + y*xA) / det;
 
-  if (A>=0 && B>=0 && Number.isInteger(A) && Number.isInteger(B)) tokens.push(A*3 + B);
+  if (A>=0 && B>=0 && Number.isInteger(A) && Number.isInteger(B)) tokens+=A*3+B;
 }
 
 console.timeEnd();
-console.log(tokens.reduce((a,b)=>a+b));
+console.log(tokens);
